f57186dc11a37a69d97ffdb0af1db5024fa97edf
Lucas Flóro <lucas@fearp.usp.br>
2018-08-20 16:47:49 -0300

    Adicionadas mensagens de soma, subtração e multiplicação antes dos resultados.

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,19 @@
 num1 = 1
 num2 = 2
 
+print("Somando 1 + 2: ")
 print(num1 + num2)
 
 #subtração
 
 sub1 = 1
 sub2 = 2
+print("Subtraindo 2 - 1: ")
 print(sub2 - sub1)
 
 
 #multiplicação
 mul1 = 2
 mul2 = 3
+print("Multiplicando 3 * 2: ")
 print(mul1 * mul2)
